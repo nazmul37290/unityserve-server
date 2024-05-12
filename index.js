da822f69039536aa4949c908c2a71da7e65d083a
nazmul37290 <nazmulnjm@gmail.com>
2024-05-12 18:49:16 +0600

    created add post api

--- index.js
+++ index.js
@@ -44,6 +44,23 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/posts", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+
+      const result = await postCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.delete("/posts/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+
+      const result = await postCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/postDetails/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
