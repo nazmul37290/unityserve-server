3714c93899c6efcc9fb4c95eb5196a25b96086bd
nazmul37290 <nazmulnjm@gmail.com>
2024-05-12 10:40:24 +0600

    added get api for posts

--- index.js
+++ index.js
@@ -26,15 +26,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    const postCollection = client.db("unityServe").collection("allNeedPosts");
+    app.get("/posts", async (req, res) => {
+      const query = postCollection.find();
+      const result = await query.toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
