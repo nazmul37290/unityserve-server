c976d5654011506f2b7ebfab6f3601a2c640930b
nazmul37290 <nazmulnjm@gmail.com>
2024-05-13 23:14:47 +0600

    added jwt token verify

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+// middlewares
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("value of token in middle ware", token);
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    req.user = decoded;
+
+    next();
+  });
+};
 
 app.get("/", (req, res) => {
   res.send("Unity server is running");
@@ -34,14 +60,34 @@ async function run() {
     );
     const requestCollection = client.db("unityServe").collection("allRequests");
     const postCollection = client.db("unityServe").collection("allNeedPosts");
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const loggedUser = req.user;
+
+      const token = jwt.sign(
+        {
+          user,
+        },
+        process.env.JWT_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production" ? true : false,
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      });
+      res.send({ success: true });
+    });
+
     app.get("/posts", async (req, res) => {
       const search = req.body;
-      console.log(search);
+
       let query = {};
-      console.log(req.query.title);
-      if (req.query?.email) {
-        query = { "organizer.email": req.query?.email };
-      }
+
+      //   if (req.query?.email) {
+      //     query = { "organizer.email": req.query?.email };
+      //   }
       if (req.query?.title) {
         const searchTitle = new RegExp(req.query?.title, "i");
         query = { title: { $regex: searchTitle } };
@@ -53,8 +99,22 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/myPosts", verifyToken, async (req, res) => {
+      console.log("from my posts", req.user);
+      console.log("from my posts 2", req.query.email);
+      //   if (req.user.email !== req.query.email) {
+      //     return res.status(403).send({ message: "forbidden" });
+      //   }
+      const query = { "organizer.email": req.query?.email };
+      const options = {
+        sort: { deadline: 1 },
+      };
+      const cursor = postCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.post("/posts", async (req, res) => {
+    app.post("/posts", verifyToken, async (req, res) => {
       const data = req.body;
       console.log(data);
 
@@ -62,7 +122,7 @@ async function run() {
       res.send(result);
     });
 
-    app.put("/posts/:id", async (req, res) => {
+    app.put("/posts/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const post = req.body;
       console.log(post);
@@ -93,7 +153,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/posts/:id", async (req, res) => {
+    app.delete("/posts/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
 
       const query = { _id: new ObjectId(id) };
@@ -102,7 +162,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/postDetails/:id", async (req, res) => {
+    app.get("/postDetails/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const query = { _id: new ObjectId(id) };
@@ -111,14 +171,19 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/volunteerRequests", async (req, res) => {
+    app.get("/volunteerRequests", verifyToken, async (req, res) => {
+      console.log("requested user", req.user);
+      console.log("query", req.query.email);
+      //   if (req.query.email !== req.user.email) {
+      //     return res.status(403).send({ message: "access forbidden" });
+      //   }
       const query = { volunteerEmail: req.query?.email };
       const cursor = requestCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.post("/beAVolunteer", async (req, res) => {
+    app.post("/beAVolunteer", verifyToken, async (req, res) => {
       const data = req.body;
       console.log(data);
 
@@ -138,7 +203,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/beAVolunteer/:id", async (req, res) => {
+    app.delete("/beAVolunteer/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const query = { _id: new ObjectId(id) };
